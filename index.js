1ce13ceb1d20efca810ecd368d56b58cd31ab2c7
Muhammad Usman <muhammad.usman@bitflex.com>
2025-02-03 19:52:38 +0500

    Simple Node Http Server

---
+++ index.js
@@ -0,0 +1,43 @@
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
+
+const myServer = http.createServer((req, res) => {
+  //Not log this request
+  if (req.url === "/favicon.ico") return res.end();
+
+  //Log incoming request
+  const log = `${Date.now()}: ${req.method}: ${
+    req.url
+  }: New request received\n`;
+
+  //Parse url
+  const parseURL = url.parse(req.url);
+  console.log("parsed URL components: ", parseURL);
+
+  fs.appendFile("log.txt", log, (err, data) => {
+    switch (parseURL.path) {
+      case "/":
+        res.end("Home page");
+        break;
+      case "/about":
+        res.end("About page: I am developer");
+        break;
+      case "/signup":
+        if (req.method === "Get") {
+          res.end("This is a signup form.");
+        } else if (req.method === "Post") {
+          //Db query
+          res.end("successfully");
+        }
+      default:
+        res.end("404 Not Found");
+        break;
+    }
+  });
+
+  //Get request header info
+  //console.log(req.headers);
+});
+
+myServer.listen(8004, () => "Server started!");
